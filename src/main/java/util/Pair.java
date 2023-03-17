789e2bfc6503f9f716f97d489c703816ceb7bb78
Joshua Turcotti <jturcotti@cs.cornell.edu>
2023-03-17 12:09:08 -0400

    modify pair

--- src/main/java/util/Pair.java
+++ src/main/java/util/Pair.java
@@ -11,4 +11,7 @@ public record Pair<L, R>(L left, R right) {
         return new Pair<>(left, foo.apply(right));
     }
 
+    public static <L, R> Pair<L, R> of(L left, R right) {
+        return new Pair<>(left, right);
+    }
 }
